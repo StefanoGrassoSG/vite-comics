74387aa421523f7970f5729bd0151f54879df249
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-07-13 18:15:04 +0200

    ADD big logo

--- src/App.vue
+++ src/App.vue
@@ -100,20 +100,38 @@ export default {
             DC COMICS
           </h3>
           <ul>
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>Tv</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
+            <li>
+              <a href="">Characters</a>
+            </li>
+            <li>
+              <a href="">Comics</a>
+            </li>
+            <li>
+              <a href="">Movies</a>
+            </li>
+            <li>
+              <a href="">Tv</a>
+            </li>
+            <li>
+              <a href="">Games</a>
+            </li>
+            <li>
+              <a href="">Videos</a>
+            </li>
+            <li>
+              <a href="">News</a>
+            </li>
           </ul>
           <h3>
             SHOP
           </h3>
           <ul>
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
+            <li>
+              <a href="">Shop DC</a>
+            </li>
+            <li>
+              <a href="">Shop DC Collectibles</a>
+            </li>
           </ul>
         </div>
         <div class="col-2">
@@ -121,17 +139,41 @@ export default {
             DC
           </h3>
           <ul>
-            <li>Terms Of Use</li>
-            <li>Privacy policy(New)</li>
-            <li>Ad choices</li>
-            <li>Advertising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
+            <li>
+              <a href="">Terms Of Use</a>
+            </li>
+            <li>
+              <a href="">Privacy policy(New)</a>
+            </li>
+            <li>
+              <a href="">
+                Ad choices
+              </a>
+            </li>
+            <li>
+              <a href="">Advertising</a>
+            </li>
+            <li>
+              <a href="">Jobs</a>
+            </li>
+            <li>
+              <a href="">Subscriptions</a>
+            </li>
+            <li>
+              <a href="">Talent Workshops</a>
+            </li>
+            <li>
+              <a href="">CPSC Certificates</a>
+            </li>
+            <li>
+              <a href="">Ratings</a>
+            </li>
+            <li>
+              <a href="">Shop Help</a>
+            </li>
+            <li>
+              <a href="">Contact Us</a>
+            </li>
           </ul>
         </div>
         <div class="col-3">
@@ -139,18 +181,32 @@ export default {
             SITES
           </h3>
           <ul>
-            <li>DC</li>
-            <li>MAD Magazine</li>
-            <li>DC Kids</li>
-            <li>DC Universe</li>
-            <li>DC Power Visa</li>
+            <li>
+              <a href="">DC</a>
+            </li>
+            <li>
+              <a href="">MAD Magazine</a>
+            </li>
+            <li>
+              <a href="">DC Kids</a>
+            </li>
+            <li>
+              <a href="">DC Universe</a>
+            </li>
+            <li>
+              <a href="">DC Power Visa</a>
+            </li>
           </ul>
         </div>
       </div>
 
       <div class="big-logo">
-
+        
       </div>
+    </div>
+
+    <div class="footer-bottom">
+
     </div>
   </footer>
 </template>
