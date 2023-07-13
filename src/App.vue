5a90ae131e718e10c6831f04d29746234bdfd1b8
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-07-13 17:30:30 +0200

    ADD blue section products

--- src/App.vue
+++ src/App.vue
@@ -50,32 +50,42 @@ export default {
       <ul>
         <li>
           <a href="">
-            <img src="" alt="">
-            <span></span>
+            <div class="buy">
+              <img src="../src/assets/img/buy-comics-digital-comics.png" alt="">
+            </div>
+            <span>DIGITAL COMICS</span>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="" alt="">
-            <span></span>
+            <div class="buy">
+              <img src="../src/assets/img/buy-comics-merchandise.png" alt="">
+            </div>
+            <span>DC MERCHANDISE</span>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="" alt="">
-            <span></span>
+            <div class="buy">
+              <img src="../src/assets/img/buy-comics-subscriptions.png" alt="">
+            </div>
+            <span>SUBSCRIPTION</span>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="" alt="">
-            <span></span>
+            <div class="buy">
+              <img src="../src/assets/img/buy-comics-shop-locator.png" alt="">
+            </div>
+            <span>COMIC SHOP LOCATOR</span>
           </a>
         </li>
         <li>
           <a href="">
-            <img src="" alt="">
-            <span></span>
+            <div class="buy">
+              <img src="../src/assets/img/buy-dc-power-visa.svg" alt="">
+            </div>
+            <span>DC POWER VISA</span>
           </a>
         </li>
       </ul>
