04df24ffe84d85cd7ce52b8571db5c96ff985993
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-07-13 17:53:35 +0200

    ADD footer-top

--- src/App.vue
+++ src/App.vue
@@ -93,7 +93,65 @@ export default {
   </section>
 
   <footer>
-    FOOTER
+    <div class="footer-top">
+      <div class="links">
+        <div class="col-1">
+          <h3>
+            DC COMICS
+          </h3>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+          <h3>
+            SHOP
+          </h3>
+          <ul>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <h3>
+            DC
+          </h3>
+          <ul>
+            <li>Terms Of Use</li>
+            <li>Privacy policy(New)</li>
+            <li>Ad choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h3>
+            SITES
+          </h3>
+          <ul>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="big-logo">
+
+      </div>
+    </div>
   </footer>
 </template>
 
